8a5fe000b9136d30a0ee69ae756beb1cf1c98b2e
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-07-11 23:37:38 +0300

    style finished, adding animation
    continue working

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+gsap.from('#card1', {
+    x: -700,
+    ease: "elastic.out(1, 2)",
+    duration: 2,
+    delay: 0.1,
+});
+gsap.from('#card3', {
+    x: 700,
+    ease: "elastic.out(1, 2)",
+    duration: 2,
+    delay: 0.1,
+
+});
+gsap.from('#card2', {
+    y: 1300,
+    duration: 1,
+
+});
+
 const toggle = document.getElementById('toggle');
 const toggleBall = document.querySelector('.toggle__trg_ball');
 const priceLow = document.querySelectorAll('.card__price_small');
